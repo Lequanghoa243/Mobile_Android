98b66cebee94a8a3ebd768a38ba0dba7e7d97c13
Lequanghoa243 <115691400+Lequanghoa243@users.noreply.github.com>
2024-05-15 02:05:43 +0700

    create SignUp

--- app/src/main/java/com/example/mobileproject/Common/SignUp.java
+++ app/src/main/java/com/example/mobileproject/Common/SignUp.java
@@ -1,6 +1,5 @@
 package com.example.mobileproject.Common;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -11,20 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.mobileproject.R;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_retailer_login);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public void login(){
-
     }
 }
