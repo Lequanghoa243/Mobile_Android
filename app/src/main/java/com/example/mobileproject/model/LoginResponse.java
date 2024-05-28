93036673231da403a6a362848804df90c1b3eae9
Lequanghoa243 <115691400+Lequanghoa243@users.noreply.github.com>
2024-05-28 18:29:34 +0700

    add in login screen

---
+++ app/src/main/java/com/example/mobileproject/model/LoginResponse.java
@@ -0,0 +1,50 @@
+package com.example.mobileproject.model;
+
+public class LoginResponse {
+    private String _id;
+    private String firstname;
+    private String lastname;
+    private String mobile;
+    private String token;
+
+    // Getters and Setters
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
